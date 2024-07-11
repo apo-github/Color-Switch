e3abd731a6c4a750830544f74eced2abe449c978
maru1129 <fufujijimamaruru@gmail.com>
2024-07-11 23:43:28 +0900

    hotfix AWSマネジメント用の背景変更機能を追加

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,2 +1,5 @@
 console.log("js読み込んだよ")
-document.body.style.backgroundColor = "gold";
+// document.body.style.backgroundColor = "gold";
+
+// AWSマネジメントコンソールの背景を変更
+document.querySelector(".globalNav-122").style.backgroundColor = "crimson";
