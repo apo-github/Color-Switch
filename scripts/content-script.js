8449c8f32fd5c1e466d9a2bbbf2a090c9c354c98
apo-github <fukick@gmail.com>
2024-07-14 20:38:50 +0900

    本番環境の時だけ色を変えるようにif文を追加

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -1,15 +1,16 @@
 console.log("Loaded JS");
 
-const USER_NAME = "user_name @ xxxx-yyyy-zzzz"
-const USER_NAME_SELECTOR = "#nav-usernameMenu"
+const USER_NAME = "work_user @ 8517-2562-3947"; //851725623947
+const USER_NAME_CSS_SELECTOR = "#nav-usernameMenu";
 const HEADER_COLOR = "crimson"; //crimson, green, navy
 const HEADER_CSS_SELECTOR = ".globalNav-122";
 const set_interval_id = setInterval(findTargetElement, 1000);
 
 function findTargetElement() {
+    console.log("loading page....");
     if(document.querySelector( HEADER_CSS_SELECTOR) != null) {
         console.log("user check!")
-        const get_user_name = document.querySelector(USER_NAME_SELECTOR).firstElementChild.textContent
+        const get_user_name = document.querySelector(USER_NAME_CSS_SELECTOR).firstElementChild.textContent
         if (get_user_name == USER_NAME){
             console.log("changed the header color!");
             document.querySelector( HEADER_CSS_SELECTOR).style.backgroundColor = HEADER_COLOR;
