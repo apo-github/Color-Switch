9e6fa75bc81fc25b4800e13dcb642a8ebace5c87
maru1129 <fufujijimamaruru@gmail.com>
2024-08-11 21:36:27 +0900

    functionFiexd: URL複数対応（初期ロード時）

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,10 +1,3 @@
-const HEADER_COLOR = "crimson"; //crimson, green, navy
-const QUERY_SELECTOR = ".globalNav-123";
-const DEFAULT_URL = "https://*.console.aws.amazon.com/*";
-const AWS_ID = "851725623947"; //851725623947
-
-
-
 getParams(); //初期ロード時の設定読み込み
 save_button = document.querySelector("#save");
 save_button.addEventListener( "click", () => {
@@ -22,16 +15,14 @@ save_button.addEventListener( "click", () => {
 
 function getParams() {
     // storage.sync.get()値がなければデフォルト値が採用される
-    chrome.storage.sync.get({
-        url_row_1: DEFAULT_URL,
-        query_selector_row_1: QUERY_SELECTOR,
-        color_row_1: HEADER_COLOR,
-        aws_id_row_1: AWS_ID
-    }, function (datas) { 
-        document.querySelector("#url-row-1").value = datas.url_row_1;
-        document.querySelector("#css-selector-row-1").value = datas.query_selector_row_1;
-        document.querySelector("#color-row-1").value = datas.color_row_1;
-        document.querySelector("#aws-id-row-1").value = datas.aws_id_row_1;        
+    chrome.storage.sync.get(null, function (datas) { 
+        const data_length = Object.keys(datas).length;
+        for (i = 1; i <= data_length; i++) {
+            document.querySelector(`#url-row-${i}`).value = datas[`url_row_${i}`];
+            document.querySelector(`#css-selector-row-${i}`).value = datas[`query_selector_row_${i}`];
+            document.querySelector(`#color-row-${i}`).value = datas[`color_row_${i}`];
+            document.querySelector(`#id-row-${i}`).value = datas[`id_row_${i}`];        
+        }
     });
 }
 
@@ -40,13 +31,13 @@ function setParams(){
     const URL_1 = document.querySelector("#url-row-1").value;
     const QUERY_SELECTOR_1 = document.querySelector("#css-selector-row-1").value;
     const COLOR_1 = document.querySelector("#color-row-1").value;
-    const AWS_ID_1 = document.querySelector("#aws-id-row-1").value;
+    const ID_1 = document.querySelector("#id-row-1").value;
 
     chrome.storage.sync.set({
-        url_row_1: URL_1,
-        query_selector_row_1: QUERY_SELECTOR_1,
-        color_row_1:COLOR_1,
-        aws_id_row_1: AWS_ID_1,
+        "url_row_1": URL_1,
+       "query_selector_row_1": QUERY_SELECTOR_1,
+        "color_row_1":COLOR_1,
+        "id_row_1": ID_1,
     }, function () {
         alert("Saved your settings(｀・ω・´)");
     });
