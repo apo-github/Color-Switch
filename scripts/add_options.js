9191b0ab2037c5017c1654f81fe23bd5119b2086
maru1129 <fufujijimamaruru@gmail.com>
2024-08-11 16:13:41 +0900

    bugFixed: ボックスの追加時dを動的に設定されるよう修正

--- scripts/add_options.js
+++ scripts/add_options.js
@@ -1,9 +1,66 @@
-document.querySelector(".btn-outline-secondary").addEventListener('click', function(){
-    // 複製元の要素
-    let originalBlock = document.querySelectorAll('.row')[1];
-    
-    // 複製（trueを渡すことで子要素含めた複製になる）
-    let cloneBlock = originalBlock.cloneNode(true);
-    originalBlock.insertAdjacentElement('afterend', cloneBlock);
+let rowNumber = document.querySelectorAll(".row").length+1;
 
+document.querySelector(".btn-outline-secondary").addEventListener('click', function(){
+
+    const URL = `url-row-${rowNumber}`;
+    const CSS_SELECTOR = `css-selector-row-${rowNumber}`;
+    const COLOR = `color-row-${rowNumber}`;
+    const SERVICE = `service-row-${rowNumber}`
+    const ID = `id-row-${rowNumber}`;
+
+    const newRowHTML = `
+      <div class="row">
+        <div class="col-5">
+          <div class="mb-3">
+              <input
+              class="form-control"
+              id="${URL}"
+              placeholder=""
+              />
+          </div>
+        </div>
+        <div class="col-2 pe-0">
+            <div class="mb-3">
+                <input
+                class="form-control" 
+                id="${CSS_SELECTOR}"
+                placeholder=""
+                />
+            </div>
+        </div>
+        <div class="col ps-0">
+          <div class="mb-3">
+              <select id="${COLOR}" class="form-select form-select">
+                <option value="blue">Blue</option>
+                <option value="green">Green</option>
+                <option value="#CC9900">Gold</option>
+                <option value="crimson">Crimson</option>
+              </select>
+          </div>
+        </div>
+        <div class="col pe-0">
+          <div class="mb-3">
+              <select id="${SERVICE}" class="select-service form-select form-select">
+                <option value="none"></option>
+                <option value="aws">AWS</option>
+                <option value="azure">Azure</option>
+              </select>
+          </div>
+        </div>
+        <div class="col-2 ps-0">
+          <div class="mb-3">
+              <input
+                class="form-control"
+                id="${ID}"
+                placeholder=""
+                />
+          </div>
+        </div>
+      </div>
+    `;
+    
+    const plusButton = document.querySelector("#plus");
+    plusButton.insertAdjacentHTML('beforebegin', newRowHTML);
+
+    rowNumber++;
 })
